ca590e21ecce1a2324f48f7db51ffc4d296e7ddd
Isaac Rudy <isaac.rudy@gmail.com>
2022-03-01 13:39:40 -0800

    added main page, general header and footer. Also a css styles sheet for them.

---
+++ scripts/skeleton.js
@@ -0,0 +1,5 @@
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./Text/GlobalNav.html'));
+    console.log($('#footerPlaceholder').load('./Text/GlobalFooter.html'));
+}
+loadSkeleton();
