3ed218bd13fc437b1be89946355dd70e55f7dedd
Isaac Rudy <isaac.rudy@gmail.com>
2022-03-22 13:33:33 -0700

    Added Guides page, feedback tab, adn insurance links

--- scripts/mainScript.js
+++ scripts/mainScript.js
@@ -8,10 +8,20 @@ function insertContent(){
                 document.getElementById("myContentText").innerHTML = title.data().Text;
                 var icon = document.getElementById("myContentImage");
                 icon.innerHTML += "<img src= './images/user.png' height='80'>"
+
+            })
+
+            db.collection("Main").doc("Feedback")
+            .onSnapshot(title => {
+                document.getElementById("feedbackText").innerHTML = title.data().Text;
+                var icon2 = document.getElementById("feedbackImage");
+                icon2.innerHTML += "<img src= './images/feedback.png' height='80'>"
             })
         } else {
             document.getElementById("myContentText").innerHTML = null;
             document.getElementById("myContentImage").innerHTML = null;
+            document.getElementById("feedbackImage").innerHTML = null;
+            document.getElementById("feedbackText").innerHTML = null;
         }
     })
 }
